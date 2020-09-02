8bc2893bb3a7a1dbbc63d3f1caf16dbaf0d4d2be
Braisdom <yonghe.wang@joowing.com>
2020-09-02 15:57:01 +0800

    Init

--- src/main/java/com/github/braisdom/objsql/intellij/ObjSqlPsiAugmentProvider.java
+++ src/main/java/com/github/braisdom/objsql/intellij/ObjSqlPsiAugmentProvider.java
@@ -1,10 +1,14 @@
 package com.github.braisdom.objsql.intellij;
 
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.RecursionGuard;
+import com.intellij.openapi.util.RecursionManager;
 import com.intellij.psi.*;
 import com.intellij.psi.augment.PsiAugmentProvider;
 import com.intellij.psi.impl.source.PsiExtensibleClass;
 import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.util.CachedValueProvider;
+import com.intellij.psi.util.PsiModificationTracker;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -24,10 +28,17 @@ public class ObjSqlPsiAugmentProvider extends PsiAugmentProvider {
         final PsiClass psiClass = (PsiClass) element;
         final Project project = element.getProject();
 
+        if (psiClass.isAnnotationType() || psiClass.isInterface()) {
+            return result;
+        }
+
         PsiAnnotation psiAnnotation = PsiAnnotationSearchUtil.findAnnotation(psiClass,
                 "com.github.braisdom.objsql.annotations.DomainModel");
 
-        if (type == PsiMethod.class && psiAnnotation != null) {
+        if(psiAnnotation == null)
+            return result;
+
+        if (type == PsiMethod.class) {
             ObjSqlLightMethodBuilder methodBuilder = new ObjSqlLightMethodBuilder(psiClass.getManager(), "createQuery");
             methodBuilder.withContainingClass((PsiClass) element)
                     .withMethodReturnType(PsiType.getTypeByName("com.github.braisdom.funcsql.Query", project,
@@ -39,6 +50,51 @@ public class ObjSqlPsiAugmentProvider extends PsiAugmentProvider {
         }
 
         return result;
+    }
+
+    private static class FieldObjSqlCachedValueProvider<Psi extends PsiElement> extends ObjSqlCachedValueProvider<Psi> {
+        private static final RecursionGuard ourGuard = RecursionManager.createGuard("lombok.augment.field");
+
+        FieldObjSqlCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
+            super(type, psiClass, ourGuard);
+        }
+    }
+
+    private static class MethodObjSqlCachedValueProvider<Psi extends PsiElement> extends ObjSqlCachedValueProvider<Psi> {
+        private static final RecursionGuard ourGuard = RecursionManager.createGuard("lombok.augment.method");
+
+        MethodObjSqlCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
+            super(type, psiClass, ourGuard);
+        }
+    }
+
+    private static class ClassObjSqlCachedValueProvider<Psi extends PsiElement> extends ObjSqlCachedValueProvider<Psi> {
+        private static final RecursionGuard ourGuard = RecursionManager.createGuard("lombok.augment.class");
+
+        ClassObjSqlCachedValueProvider(Class<Psi> type, PsiClass psiClass) {
+            super(type, psiClass, ourGuard);
+        }
+    }
+
+    private abstract static class ObjSqlCachedValueProvider<Psi extends PsiElement> implements CachedValueProvider<List<Psi>> {
+        private final Class<Psi> type;
+        private final PsiClass psiClass;
+        private final RecursionGuard recursionGuard;
+
+        ObjSqlCachedValueProvider(Class<Psi> type, PsiClass psiClass, RecursionGuard recursionGuard) {
+            this.type = type;
+            this.psiClass = psiClass;
+            this.recursionGuard = recursionGuard;
+        }
+
+        @Override
+        public Result<List<Psi>> compute() {
+            return recursionGuard.doPreventingRecursion(psiClass, true, () -> {
+                final List<Psi> result = new ArrayList<>();
+
+                return Result.create(result, PsiModificationTracker.JAVA_STRUCTURE_MODIFICATION_COUNT);
+            });
+        }
     }
 
 }
