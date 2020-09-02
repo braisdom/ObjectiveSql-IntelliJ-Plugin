99e947b991e7cc93b5a86c8a393655378930d59a
Braisdom <yonghe.wang@joowing.com>
2020-09-02 18:08:17 +0800

    Init

--- src/main/java/com/github/braisdom/objsql/intellij/PrimaryBuilder.java
+++ src/main/java/com/github/braisdom/objsql/intellij/PrimaryBuilder.java
@@ -1,4 +1,12 @@
 package com.github.braisdom.objsql.intellij;
 
-public class PrimaryBuilder {
+import com.intellij.psi.PsiClass;
+
+import java.util.List;
+
+final class PrimaryBuilder {
+
+    static void buildPrimaryField(PsiClass psiClass, List result) {
+
+    }
 }
