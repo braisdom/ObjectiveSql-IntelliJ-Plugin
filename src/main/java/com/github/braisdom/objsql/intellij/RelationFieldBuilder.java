f99692ca3b04d4f0b8042ced8456b061277a2fef
Braisdom <yonghe.wang@joowing.com>
2020-09-02 22:42:49 +0800

    Add relationship builder

--- src/main/java/com/github/braisdom/objsql/intellij/RelationFieldBuilder.java
+++ src/main/java/com/github/braisdom/objsql/intellij/RelationFieldBuilder.java
@@ -1,24 +1,45 @@
 package com.github.braisdom.objsql.intellij;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.psi.PsiClass;
-import com.intellij.psi.PsiModifier;
-import com.intellij.psi.PsiType;
+import com.intellij.psi.*;
 import com.intellij.psi.impl.light.LightFieldBuilder;
 import com.intellij.psi.search.GlobalSearchScope;
 
+import java.util.Collection;
 import java.util.List;
 
 final class RelationFieldBuilder {
 
-    static void build(PsiClass psiClass, List result) {
-        Project project = psiClass.getProject();
-        PsiType primaryType = PsiType.getTypeByName("java.lang.String", project, GlobalSearchScope.allScope(project));
-        String primaryName = "DEMO";
-        LightFieldBuilder primaryBuilder = new LightFieldBuilder(primaryName, primaryType, psiClass);
-        primaryBuilder.setModifiers(PsiModifier.PUBLIC, PsiModifier.FINAL, PsiModifier.STATIC);
-        primaryBuilder.setContainingClass(psiClass);
+    private static final String RELATION_ANNOTATION = "com.github.braisdom.objsql.annotations.Relation";
 
-        result.add(primaryBuilder);
+    static void build(PsiClass psiClass, List result) {
+        final Project project = psiClass.getProject();
+        Collection<PsiField> fields = PsiClassUtil.collectClassFieldsIntern(psiClass);
+        for (PsiField field : fields) {
+            PsiAnnotation annotation = field
+                    .getAnnotation(RELATION_ANNOTATION);
+            if (annotation != null) {
+                PsiType primaryType = PsiType.getTypeByName("com.github.braisdom.objsql.relation.Relationship", project, GlobalSearchScope.allScope(project));
+                String fieldName = genFieldName(field, annotation);
+
+                LightFieldBuilder primaryBuilder = new LightFieldBuilder(fieldName, primaryType, psiClass);
+                primaryBuilder.setModifiers(PsiModifier.PUBLIC, PsiModifier.FINAL, PsiModifier.STATIC);
+                primaryBuilder.setContainingClass(psiClass);
+
+                result.add(primaryBuilder);
+            }
+        }
+    }
+
+    static String genFieldName(PsiField psiField, PsiAnnotation annotationValue) {
+        String value = annotationValue.findAttributeValue("relationType").getText();
+        if ("RelationType.HAS_MANY".equalsIgnoreCase(value)) {
+            return WordUtil.underscore(String.format("%s_%s", "HAS_MANY", psiField.getName())).toUpperCase();
+        } else if ("RelationType.HAS_ONE".equalsIgnoreCase(value)) {
+            return WordUtil.underscore(String.format("%s_%s", "HAS_ONE", psiField.getName())).toUpperCase();
+        } else if ("RelationType.BELONGS_TO".equalsIgnoreCase(value)) {
+            return WordUtil.underscore(String.format("%s_%s", "BELONGS_TO", psiField.getName())).toUpperCase();
+        }
+        return null;
     }
 }
